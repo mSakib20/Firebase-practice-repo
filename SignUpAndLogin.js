b3558dd8fb4a9bf6bee528f523d4748f99392b7d
Q-wrld97 <dough_boy97@hotmail.com>
2023-02-18 13:59:39 -0500

    login and database functionality

--- SignUpAndLogin.js
+++ SignUpAndLogin.js
@@ -14,13 +14,9 @@ function register() {
   // Move on with Auth
   auth.createUserWithEmailAndPassword(email, password)
   .then((res) => {
-    db.collection("users").add({
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
+    // refrence to the user collection and add a new document refrencing user uid and add field call "email" and pass in the email variable on input
+    db.collection('user').doc(res.user.uid).set({
+     email:email
     })
     .catch((error) => {
         console.error("Error adding document: ", error);
@@ -37,6 +33,40 @@ function register() {
     alert(error_message)
   })
 }
+
+
+// Set up our login function
+function login () {
+  // Get all our input fields
+  email = document.getElementsByName('email')[0].value
+  password = document.getElementsByName('password')[0].value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Wrong!')
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then((res) => {{
+    //once user is logged in update the login time field in the user collection
+    db.collection('user').doc(res.user.uid).update({
+      loginTime:firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    // done
+    alert('User Logged In!!') 
+  }})
+  .catch((error) => {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+
 
 // Validate Functions
 function validate_email(email) {
@@ -58,4 +88,3 @@ function validate_password(password) {
     return true
   }
 }
-
