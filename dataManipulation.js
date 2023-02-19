72eec03a17311cc4481b4f20f65d2ded00b53640
Adam Clark <adam.clark.che@gmail.com>
2023-02-19 00:11:06 -0500

    Modified update instead of add command

--- dataManipulation.js
+++ dataManipulation.js
@@ -1,17 +1,17 @@
 function addNewData(){
 //form.addEventListener('submit', (e) => {   //event name e, can be named whatever
   //  e.preventDefault(); 
-    data=document.getElementsByName('test')[0].value;
-    data2=document.getElementsByName('test2')[0].value;
+    courseName=document.getElementsByName('courseName')[0].value;
+    termLength=document.getElementsByName('termLength')[0].value;
     auth.onAuthStateChanged(function(user) {
       if (user) {
         // User is signed in.      
         alert(user.uid)
-        alert(data)
-        alert(data2)  
-        db.collection('users').doc(user.uid).add({
-          newdata: data,
-          newdata2: data2
+        alert(courseName)
+        alert(termLength)  
+        db.collection('users').doc(user.uid).update({
+          courseName: courseName,
+          termLength: termLength
       
         }).catch((error) => {
           // Firebase will use this to alert of its errors
